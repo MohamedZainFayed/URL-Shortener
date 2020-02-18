223dc3679b6f3e60d46966fc92d646b4d4f3bca5
Mohamed Zain <mohamed.z.fayed@gmail.com>
2020-02-19 01:51:15 +0200

    Add mode tests to Link model

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -15,10 +15,27 @@ RSpec.describe Link, type: :model do
     expect(subject).not_to be_valid
   end
 
+  it 'URL should be unique' do
+    link_with_same_url = Link.new(url: "https://www.facebook.com")
+    link_with_same_url.save
+    subject.url = link_with_same_url.url
+    expect(subject).not_to be_valid
+  end
+
   it 'Slug should be unique' do
     link_with_same_slug = Link.new(url: "https://www.facebook.com")
     link_with_same_slug.save
     subject.slug = link_with_same_slug.slug
+    expect(subject).not_to be_valid
+  end
+
+  it 'URL Length shoud not exceed 255 characters' do
+    subject.url = "A" * 256
+    expect(subject).not_to be_valid
+  end
+
+  it 'Slug Length shoud not exceed 255 characters' do
+    subject.slug = "A" * 256
     expect(subject).not_to be_valid
   end
 end
