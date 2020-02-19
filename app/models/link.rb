4da8b4728b9a0d8005f7039b0a2e221f16ac84a6
Mohamed Zain <mohamed.z.fayed@gmail.com>
2020-02-19 18:48:27 +0200

    Implement Link validations

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,13 @@
 class Link < ApplicationRecord
+    validates :url, presence: true, uniqueness: true, length: { within: 3..255 }
+    validates :slug, presence: true, uniqueness: true, length: { within: 3..255 }
+
+    before_validation :generate_slug
+
+    private
+    
+    def generate_slug
+        self.slug = "#{self.id}#{SecureRandom.uuid[0..7]}" if self.slug.nil? || self.slug.empty?
+        true
+    end
 end
