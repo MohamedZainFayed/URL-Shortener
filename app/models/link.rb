fbc1186652db772b0e903789e07b9199b3656894
Mohamed Zain <mohamed.z.fayed@gmail.com>
2020-02-20 01:32:43 +0200

    Fix slug generation

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,7 +3,7 @@ class Link < ApplicationRecord
     validates :slug, presence: true, uniqueness: true, length: { within: 3..255 }
 
     before_validation :generate_slug
-
+    
     def self.init user_params
         user_params[:url] = "http://#{user_params[:url].strip.downcase.gsub(/(https?:\/\/)|(www\.)/,"")}"
         link_from_db = find_by_url(user_params[:url])
@@ -14,7 +14,7 @@ class Link < ApplicationRecord
     private
     
     def generate_slug
-        self.slug = "#{self.id}#{SecureRandom.uuid[0..7]}" if self.slug.nil? || self.slug.empty?
+        self.slug = "#{Link.last.id + 1}#{SecureRandom.uuid[0..7]}" if self.slug.nil? || self.slug.empty?
         true
     end
 end
