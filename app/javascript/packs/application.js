92bda87b9498c37cd1559f09b75c59b4a907c76e
Mohamed Zain <mohamed.z.fayed@gmail.com>
2020-02-20 01:33:50 +0200

    Add Bootsrtap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,9 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
+require("jquery3")
+require("popper")
+require("bootstrap-sprockets")
 require("channels")
 
 
