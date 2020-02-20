722409c8af6b0bb99ec167c9153bbb9fb9324f51
Mohamed Zain <mohamed.z.fayed@gmail.com>
2020-02-20 21:48:53 +0200

    Add feature integration tests

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe LinksController, type: :controller do
+RSpec.describe LinksController, type: :request do
+    describe "Test feature integration" do
+        it "Root path should have a form to create URL and redirects correctly" do
+            visit root_path
+            fill_in "link[url]", with: "www.gmail.com"
+            expect { click_button "commit" }.to change(Link, :count).by(1)
+            expect(page).to have_content("http://gmail.com")
+        end
 
+        it "Duplicated link shoud not add new record to the database" do
+            visit root_path
+            fill_in "link[url]", with: "www.google.com"
+            expect { click_button "commit" }.to change(Link, :count).by(1)
+            expect(page).to have_content("http://google.com")
+            
+            urls = [
+                "https://google.com",
+                "http://google.com",
+                "https://www.google.com",
+                "www.google.com",
+                "google.com"
+            ]
+
+            urls.each do |url|
+                visit root_path
+                fill_in "link[url]", with: url
+                expect { click_button "commit" }.to change(Link, :count).by(0)
+                expect(page).to have_content("http://google.com")
+            end
+        end
+        
+        it "Invalid short links should raise Not Found error" do
+            expect { visit "/invalid_slug" }.to raise_error("Not Found")
+        end
+    end
 end
